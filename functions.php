82f739454d15fb51f604f060b99861b593d3bcfd
Sobenkov <Sobenkov95@yandex.ru>
2020-02-16 13:59:20 +0300

    WPTest02

--- functions.php
+++ functions.php
@@ -2,23 +2,26 @@
 
 add_action('wp_enqueue_scripts', 'style_theme');
 add_action('wp_footer', 'scripts_theme');
-add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+add_action('after_setup_theme', 'theme_register_nav_menu');
 
 function style_theme(){
    wp_enqueue_style('style', get_stylesheet_uri());
    wp_enqueue_style( 'default', get_template_directory_uri() . '/assets/css/default.css');
    wp_enqueue_style( 'layout', get_template_directory_uri() . '/assets/css/layout.css');
    wp_enqueue_style( 'media-queries', get_template_directory_uri() . '/assets/css/media-queries.css');
-};
+}
 
 function scripts_theme(){
-   wp_enqueue_style('init', get_template_directory_uri() . '/js/init.js' );
-   wp_enqueue_style('doubletaptogo', get_template_directory_uri() . '/js/doubletaptogo.js' );
-   wp_enqueue_style('flexslider', get_template_directory_uri() . '/js/jquery.flexslider.js' );
-};
+   wp_deregister_script( 'jquery' );
+   wp_register_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
+   wp_enqueue_script( 'jquery' );
+   wp_enqueue_script('flexslider', get_template_directory_uri() . '/assets/js/jquery.flexslider.js', ['jquery'], null, true);
+   wp_enqueue_script('doubletaptogo', get_template_directory_uri() . '/assets/js/doubletaptogo.js', ['jquery'], null, true);
+   wp_enqueue_script('init', get_template_directory_uri() . '/assets/js/init.js', ['jquery'], null, true );
+   wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/js/modernizr.js', ['jquery'], null, null, false );
+}
 
-function my_scripts_method() {
-	wp_deregister_script( 'jquery-core' );
-	wp_register_script( 'jquery-core', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js');
-	wp_enqueue_script( 'jquery' );
-};
+function theme_register_nav_menu(){
+   register_nav_menu('primary', 'Primary Menu');
+   register_nav_menu('footer', 'Footer Menu');
+}
