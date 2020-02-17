975a983850b90ad356c5207f5d9d568c41c371f3
Sobenkov <Sobenkov95@yandex.ru>
2020-02-17 13:45:04 +0300

    WPTest03

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 add_action('wp_enqueue_scripts', 'style_theme');
 add_action('wp_footer', 'scripts_theme');
 add_action('after_setup_theme', 'theme_register_nav_menu');
+add_action('widgets_init', 'register_my_widgets');
 
 function style_theme(){
    wp_enqueue_style('style', get_stylesheet_uri());
@@ -24,4 +25,17 @@ function scripts_theme(){
 function theme_register_nav_menu(){
    register_nav_menu('primary', 'Primary Menu');
    register_nav_menu('footer', 'Footer Menu');
+}
+
+function register_my_widgets(){
+   	register_sidebar( array(
+		'name'          => 'Right Sidebar',
+		'id'            => "right_sidebar",
+		'description'   => '',
+		'class'         => '',
+		'before_widget' => '<div class="widget %2$s">',
+		'after_widget'  => "</div>\n",
+		'before_title'  => '<h5 class="widgettitle">',
+		'after_title'   => "</h5>\n",
+	) );
 }
